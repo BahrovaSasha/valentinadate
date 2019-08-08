c28e2d67daeb8a54fa0bb263e97e0d60c66fefc5
BahrovaSasha <bagrovaalexandra@gmail.com>
2019-08-08 17:35:32 +0300

    all responsive+js

--- app/js/common.js
+++ app/js/common.js
@@ -2,28 +2,59 @@ $(window).on('load', function() {
     $('.preloader').delay(1000).fadeOut('fast');
 });
 $(document).ready(function(){
-  /*  $('.achievements-items').slick({
+    $('.achievements-items, .about-us-items, .article-items').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 5000,
         arrows: false,
         dots: false,
-    });*/
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 
     $('body').addClass('fixed');
     $('#burger,.discover-button').click(function(){
         $('.main-header').toggleClass('open-menu').removeClass('active');
-        $('body').toggleClass('fixed');
+        // $('body').addClass('fixed');
         $('.menu-open-items').toggleClass('active');
         $('.menu-item').on('click', function(){
             $('.menu-open-items').removeClass('active');
-            $('body').removeClass('fixed');
-
             $('.main-header').addClass('active').removeClass('open-menu');
-
+            $('body').removeClass('fixed');
         });
     });
 
-    
+    $(window).scroll(function() {
+        if ( $(this).scrollTop() >= 100 ) {
+            $('.main-header').addClass('active')
+        } else if ( $(this).scrollTop() <= 100 ) {
+            $('.main-header').removeClass('active');
+        } else {
+            $('main-header').addClass('active');
+        }
+    });
+
+    $('.logo-wrap').click(function(){
+        $('body,html').animate({
+            scrollTop: $('.main-section').offset().top - 100
+        }, 800);
+        $('body').addClass('fixed');
+    });
+
 });
