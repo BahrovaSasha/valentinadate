55b2f6940452793c05053d2b79128c37d45e5d6a
BahrovaSasha <bagrovaalexandra@gmail.com>
2019-08-22 14:51:29 +0300

    js

--- app/js/common.js
+++ app/js/common.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
     $('body').addClass('fixed');
     $('#burger,.discover-button').click(function(){
         $('.main-header').toggleClass('open-menu').removeClass('active');
-        // $('body').addClass('fixed');
+        $('body').addClass('fixed');
         $('.menu-open-items').toggleClass('active');
         $('.menu-item').on('click', function(){
             $('.menu-open-items').removeClass('active');
@@ -41,14 +41,15 @@ $(document).ready(function(){
     });
 
     $(window).scroll(function() {
-        if ( $(this).scrollTop() >= 100 ) {
-            $('.main-header').addClass('active')
-        } else if ( $(this).scrollTop() <= 100 ) {
+        if ($(this).scrollTop() >= 100) {
+            $('.main-header').addClass('active');
+        } else if ($(this).scrollTop() <= 100) {
             $('.main-header').removeClass('active');
         } else {
             $('main-header').addClass('active');
         }
     });
+
 
     $('.logo-wrap').click(function(){
         $('body,html').animate({
@@ -57,9 +58,7 @@ $(document).ready(function(){
         $('body').addClass('fixed');
     });
 
-    $('.item-name').click(function() {
-        $('body').removeClass('fixed');
-    });
+
     $('tour-packages-table').removeClass('premium-active');
     $('.basic-btn').click(function () {
         $('.tour-packages-table').toggleClass('basic-active');
